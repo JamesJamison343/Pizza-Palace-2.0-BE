e6726f0d18f01e0d57e9b3bc9a9e3e5ba30876a5
Michael Whitaker <miwhitaker@ymail.com>
2021-12-07 16:37:33 -0600

    forgot annotations

--- PizzaPalace2BE/src/main/java/com/pizzaplace2/controller/RestaurantController.java
+++ PizzaPalace2BE/src/main/java/com/pizzaplace2/controller/RestaurantController.java
@@ -5,15 +5,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pizzapalace2.model.Restaurant;
 import com.pizzapalace2.service.RestaurantService;
 
 @RestController
+@RequestMapping(value = "/pizza")
+@CrossOrigin(origins = "*")
 public class RestaurantController {
 	
 	private RestaurantService rServ;
